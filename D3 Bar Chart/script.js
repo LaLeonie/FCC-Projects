df36eed34d6baff64b5861ddef8b167b69115585
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-09-19 13:20:53 +0200

    2nd user story: creates xaxis with id

--- D3 Bar Chart/script.js
+++ D3 Bar Chart/script.js
@@ -15,7 +15,6 @@ let width = 800;
 let height = 600;
 let padding = 40;
 
-// Selections
 let svg = d3.select("svg");
 
 let drawCanvas = () => {
@@ -23,12 +22,48 @@ let drawCanvas = () => {
   svg.attr("height", height);
 };
 
-let generateScales = () => {};
+let generateScales = () => {
+  // Create y and x axis
+  heightScale = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(values, (item) => {
+        return item[1];
+      }),
+    ])
+    .range([0, height - 2 * padding]);
+
+  xScale = d3
+    .scaleLinear()
+    .domain([0, values.length - 1])
+    .range([padding, width - padding]);
+
+  // Create scale for x axis to be drawn
+  let datesArray = values.map((item) => new Date(item[0]));
+  console.log(datesArray);
+  xAxisScale = d3
+    .scaleTime()
+    .domain([d3.min(datesArray), d3.max(datesArray)])
+    .range([padding, width - padding]);
+
+  // Create scale for y axis
+  yAxisScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(values, (item) => item[1])])
+    .range([height - padding, padding]);
+};
 
 let drawBars = () => {};
 
-// Generate x and y axis
-let generateAxes = () => {};
+let generateAxes = () => {
+  let xAxis = d3.axisBottom(xAxisScale);
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("id", "x-axis")
+    .attr("transform", "translate(0," + (height - padding) + ")");
+};
 
 // EXECUTION
 // create JS object from data
