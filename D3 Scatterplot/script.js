3b0c39af3978f1d199f8d11d1940cedef34d611d
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-09-22 09:27:03 +0200

    user story 7: aligns dots on xaxis

--- D3 Scatterplot/script.js
+++ D3 Scatterplot/script.js
@@ -25,12 +25,18 @@ const canvasContent = containerCanvas
   .attr("transform", `translate(${margin.left} ${margin.top})`);
 
 // Create scales
-const xScale = d3.scaleTime().range([0, visWidth]);
-const yScale = d3.scaleTime().range([visHeight, 0]);
+const xScale = d3.scaleLinear().range([0, visWidth]);
+const yScale = d3.scaleTime().range([0, visHeight]);
+
+//create time converter function
+const parseYear = d3.timeParse("%Y"); //converst time to Date object
+const parseTime = d3.timeParse("%M:%S");
+const timeToYear = d3.timeFormat("%Y");
+const timeToMinAndSec = d3.timeFormat("%M:%S");
 
 /* DATA SET
  * data set is array of objects with time, place, seconds, name, year
- * Turn data into array with year and times in minutes
+ * Turn data into array [time, seconds, year]
  */
 
 // Get Data
@@ -39,8 +45,54 @@ let url =
 d3.json(url).then((data) => {
   let dataset = [];
   data.map((d) => dataset.push([d.Time, d.Seconds, d.Year]));
-  console.log(dataset);
   drawChart(dataset);
 });
 
-let drawChart = (dataArray) => {};
+let drawChart = (dataArray) => {
+  /**
+   * data is an array containing 275+ arrays
+   * each data[i] array nests a three dimensional array
+   * d[i][0] contains information regarding time in min:sec format
+   * d[i][1] contains information regarding time in sec format
+   * d[i][2] contains information about the year
+   */
+
+  // format data
+  dataArray.forEach((d) => {
+    d[0] = parseTime(d[0]);
+    d[1] = new Date(d[1] * 1000);
+    d[2] = d[2];
+  });
+
+  console.log(dataArray);
+
+  //set domain
+  xScale.domain(d3.extent(dataArray, (d) => d[2]));
+  yScale.domain(d3.extent(dataArray, (d) => d[0])).tickFormat(function (d, i) {
+    return data[i];
+  });
+
+  //draw axis
+  const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+  const yAxis = d3.axisLeft(yScale);
+
+  canvasContent
+    .append("g")
+    .attr("id", "x-axis")
+    .attr("transform", `translate(0,${visHeight})`)
+    .call(xAxis);
+
+  canvasContent.append("g").attr("id", "y-axis").call(yAxis);
+
+  //draw points
+  canvasContent
+    .selectAll("circle")
+    .data(dataArray)
+    .enter()
+    .append("circle")
+    .attr("class", "dot")
+    .attr("r", "5")
+    .attr("data-xvalue", (d) => d[2])
+    .attr("data-yvalue", (d) => d[1])
+    .attr("cx", (d, i) => xScale(d[2]));
+};
